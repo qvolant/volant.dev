346f05a36d0545b681ca1678b4f3c772911045b4
Quentin Volant <quentin.volant@gmail.com>
2025-07-08 01:45:23 +0200

    First commit

---
+++ static/js/main.js
@@ -0,0 +1,20 @@
+const _root = document.querySelector(':root');
+
+document.addEventListener("htmx:load", function() {
+	setRandomColor();
+	const menuButton = document.getElementById('menu-button');
+	const sidebarNav = document.querySelector('#sidebar nav');
+	const sidebarFooter = document.querySelector('#sidebar footer');
+	let isMenuHidden = true;
+	menuButton.addEventListener('click', function() {
+		sidebarNav.style.height = isMenuHidden ? 'auto' : '0';
+		sidebarFooter.style.height = isMenuHidden ? 'auto' : '0';
+		isMenuHidden = !isMenuHidden;
+	})
+});
+
+function setRandomColor() {
+	let hue = Math.floor(360 * Math.random());
+	_root.style.setProperty('--main-color', 'hsl(' + hue + ', 50%, 75%)');
+	_root.style.setProperty('--main-color-darker', 'hsl(' + hue + ', 50%, 55%)');
+}
